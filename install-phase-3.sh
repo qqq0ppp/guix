2e995e738a5ad385c86945e4877611e708a6f5de
qqq0ppp <82241329+qqq0ppp@users.noreply.github.com>
2021-06-26 13:42:25 +0300

    Update install-phase-3.sh

--- install-phase-3.sh
+++ install-phase-3.sh
@@ -19,7 +19,9 @@ mkdir ~/.config/guix
 wget https://raw.githubusercontent.com/qqq0ppp/guix/main/channels.scm -P /home/me/.config/guix
 guix pull
 
-# Install packages
-git clone https://github.com/qqq0ppp/guix.git
-ls guix/packages/ | xargs -I tt guix package --install-from-file=guix/packages/tt
-rm -r guix
+guix install qutebrowser
+
+## Install packages
+#git clone https://github.com/qqq0ppp/guix.git
+#ls guix/packages/ | xargs -I tt guix package --install-from-file=guix/packages/tt
+#rm -r guix
